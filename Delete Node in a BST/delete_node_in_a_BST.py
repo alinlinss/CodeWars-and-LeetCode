21e635d3ebef7fb58ed38352c93500b2e7b7980d
Ari <derevyagina@ucu.edu.ua>
2026-04-21 22:46:41 +0300

    Implement Delete Node in a BST

---
+++ Delete Node in a BST/delete_node_in_a_BST.py
@@ -0,0 +1,25 @@
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        else:
+
+            if not root.left and not root.right:
+                return None
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                temp = root.right
+                while temp.left != None:
+                    temp = temp.left
+                root.val = temp.val
+                root.right = self.deleteNode(root.right, temp.val)
+
+        return root
